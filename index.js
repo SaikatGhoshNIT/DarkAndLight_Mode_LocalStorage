b766b9ef52ebaab3f0d0e8a78b1209eab5167e27
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-23 23:37:40 +0530

    fix : defined body as local variable for the functions

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function toggle() {
-  //getting current mode
-  const currentMode = localStorage.getItem("mode");
-  //changing the mode
-  const newMode = currentMode === "dark" ? "light" : "dark";
-  body.classList.remove(currentMode);
-  body.classList.add(newMode);
-  localStorage.setItem("mode", newMode);
+    const body = document.body;
+    //getting current mode
+    const currentMode = localStorage.getItem("mode");
+    //changing the mode
+    const newMode = currentMode === "dark" ? "light" : "dark";
+    body.classList.remove(currentMode);
+    body.classList.add(newMode);
+    localStorage.setItem("mode", newMode);
 }
 
 //Check and set the mode on page load with local storage
 function checkMode() {
-  const body = document.body;
-  const saveMode =
-    (localStorage.getItem("mode") === "dark") | "light"
-      ? localStorage.getItem("mode")
-      : "light";
-  body.classList.add(saveMode);
+    const body = document.body;
+    const saveMode =
+        (localStorage.getItem("mode") === "dark") | "light"
+            ? localStorage.getItem("mode")
+            : "light";
+    body.classList.add(saveMode);
 }
